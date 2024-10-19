0c99c5def52f5853a30bf761fc2b459247c0c6fb
Valeriy Remeshevskiy <remeshevskiy@gmail.com>
2024-10-19 23:46:23 +0300

    Actualize PyQt API

--- src/qgis_ogrstyle/ogrstyle_tool.py
+++ src/qgis_ogrstyle/ogrstyle_tool.py
@@ -24,9 +24,9 @@
 # ******************************************************************************
 
 from osgeo import ogr
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QApplication
+from qgis.PyQt.QtCore import *
+from qgis.PyQt.QtGui import *
+from qgis.PyQt.QtWidgets import QApplication
 from qgis.core import *
 from qgis.gui import *
 

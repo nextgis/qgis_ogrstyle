0be92e3d6075be9617c86e0774a18f15f20ef82d
Ivan Barsukov <i.barsukov@protonmail.com>
2024-11-19 21:40:02 +0700

    Update translation

--- src/qgis_ogrstyle/i18n/copy_ogr_style_ru.ts
+++ src/qgis_ogrstyle/i18n/copy_ogr_style_ru.ts
@@ -4,62 +4,106 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../about_dialog.py" line="83"/>
+        <location filename="../about_dialog.py" line="206"/>
         <source>Developers</source>
         <translation>Разработчики</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="84"/>
+        <location filename="../about_dialog.py" line="207"/>
         <source>Homepage</source>
         <translation>Домашняя страница</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="85"/>
+        <location filename="../about_dialog.py" line="208"/>
         <source>Please report bugs at</source>
         <translation>Пожалуйста, сообщайте о багах в</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="88"/>
+        <location filename="../about_dialog.py" line="211"/>
         <source>Video with an overview of the plugin</source>
         <translation>Видео с обзором на плагин</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="89"/>
+        <location filename="../about_dialog.py" line="212"/>
         <source>Other helpful services by NextGIS</source>
         <translation>Другие полезные сервисы от NextGIS</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="90"/>
+        <location filename="../about_dialog.py" line="213"/>
         <source>Convenient up-to-date data extracts for any place in the world</source>
         <translation>Удобная выборка актуальных данных из любой точки мира</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="93"/>
+        <location filename="../about_dialog.py" line="216"/>
         <source>Fully featured Web GIS service</source>
         <translation>Полнофункциональный Веб ГИС-сервис</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="87"/>
+        <location filename="../about_dialog.py" line="210"/>
         <source>bugtracker</source>
         <translation>багтрекер</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="78"/>
+        <location filename="../about_dialog.py" line="201"/>
         <source>REPORT_END</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="61"/>
+        <source> by NextGIS</source>
+        <translation> от NextGIS</translation>
     </message>
 </context>
 <context>
     <name>AboutDialogBase</name>
     <message>
-        <location filename="../about_dialog_base.ui" line="14"/>
+        <location filename="../ui/about_dialog_base.ui" line="14"/>
         <source>About {plugin_name}</source>
         <translation>О плагине {plugin_name}</translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="27"/>
-        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="33"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="72"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="94"/>
+        <source>Components</source>
+        <translation>Компоненты</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="116"/>
+        <source>Contributors</source>
+        <translation>Участники</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="158"/>
+        <source>{plugin_name}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="168"/>
+        <source>Version {version}</source>
+        <translation>Версия {version}</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="203"/>
+        <source>Get involved</source>
+        <translation>Присоединяйтесь</translation>
     </message>
 </context>
 <context>
@@ -83,7 +127,7 @@
 <context>
     <name>StyleViewerDialogBase</name>
     <message>
-        <location filename="../qgis_ogrstyle_dialog_base.ui" line="14"/>
+        <location filename="../ui/qgis_ogrstyle_dialog_base.ui" line="14"/>
         <source>Style Viewer</source>
         <translation>Просмотр стилей</translation>
     </message>
